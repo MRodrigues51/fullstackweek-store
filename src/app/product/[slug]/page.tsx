2ea97d307c512b9df94ea1028fa0b9a9afe6f23c
MRodrigues51 <marcelorodriguesweb@gmail.com>
2023-11-01 13:28:17 -0300

    feat: add recommend products title to product details page and list products recommend

--- src/app/product/[slug]/page.tsx
+++ src/app/product/[slug]/page.tsx
@@ -2,28 +2,50 @@ import { prismaClient } from "@/lib/prisma";
 import ProductImages from "./components/product-images";
 import { computeProductTotalPrice } from "@/helpers/products";
 import ProductInfo from "./components/product-info";
+import ProductList from "@/components/ui/product-list";
+import SectionTitle from "@/components/ui/section-title";
 
 interface ProductDetailsPageProps {
-    params: {
-        slug: string
-    }
+  params: {
+    slug: string;
+  };
 }
 
-const ProductDetailsPage = async ({params: {slug}}: ProductDetailsPageProps) => {
-    const product = await prismaClient.product.findFirst({
-        where: {
-            slug: slug
-        }  
-    })
-    if (!product) {
-        return <h1>Product not found</h1>
-    }
-    return ( 
-        <div className="flex flex-col gap-8">
-            <ProductImages imageUrls={product.imageUrls} name={product.name}/>
-            <ProductInfo product={computeProductTotalPrice(product)}/>
-        </div>
-    );
-}
- 
+const ProductDetailsPage = async ({
+  params: { slug },
+}: ProductDetailsPageProps) => {
+  const product = await prismaClient.product.findFirst({
+    where: {
+      slug: slug,
+    },
+    include: {
+      category: {
+        include: {
+          products: {
+            where: {
+              slug: {
+                not: slug,
+              },
+            },
+          },
+        },
+      },
+    },
+  });
+  if (!product) {
+    return <h1>Product not found</h1>;
+  }
+  return (
+    <div className="flex flex-col gap-8 pb-8">
+      <ProductImages imageUrls={product.imageUrls} name={product.name} />
+      <ProductInfo product={computeProductTotalPrice(product)} />
+
+      <div>
+        <SectionTitle>Produtos Recomendados</SectionTitle>
+        <ProductList products={product.category.products} />
+      </div>
+    </div>
+  );
+};
+
 export default ProductDetailsPage;
