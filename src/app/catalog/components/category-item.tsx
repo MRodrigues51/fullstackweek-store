bb1fed72017bf74536854a11d26cf2002dc233fd
MRodrigues51 <marcelorodriguesweb@gmail.com>
2023-10-31 21:30:22 -0300

    feat: add page show category products

---
+++ src/app/catalog/components/category-item.tsx
@@ -0,0 +1,34 @@
+import { Category } from "@prisma/client";
+import Image from "next/image";
+import Link from "next/link";
+
+interface CategoryItemProps {
+  category: Category;
+}
+
+const CategoryItem = ({ category }: CategoryItemProps) => {
+  return (
+    <Link href={`/category/${category.slug}`}>
+      <div className="flex flex-col">
+      <div className="flex h-[150px] w-full items-center justify-center rounded-tl-lg rounded-tr-lg bg-category-item-background">
+        <Image
+          src={category.imageUrl}
+          alt={category.name}
+          width={0}
+          height={0}
+          sizes="100vw"
+          className="h-auto max-h-[70%] w-auto max-w-[80%]"
+          style={{
+            objectFit: "contain",
+          }}
+        />
+      </div>
+      <div className="bg-accent py-2 rounded-bl-lg rounded-br-lg">
+        <p className="text-sm font-semibold text-center">{category.name}</p>
+      </div>
+    </div>
+    </Link>
+  );
+};
+
+export default CategoryItem;
--- src/app/catalog/page.tsx
+++ src/app/catalog/page.tsx
@@ -1,5 +1,23 @@
-const CatalogPage = () => {
-    return ( <h1>catalog</h1> );
+import { Badge } from "@/components/ui/badge";
+import { prismaClient } from "@/lib/prisma";
+import { ShapesIcon } from "lucide-react";
+import CategoryItem from "./components/category-item";
+
+const CatalogPage = async () => {
+    const categories = await prismaClient.category.findMany({
+
+    })
+    return ( 
+        <div className="p-5 flex flex-col gap-8">
+            <Badge className=" w-fit gap-1 border-primary border-2 text-base uppercase px-3 py-[0.375rem]" variant="outline">
+                <ShapesIcon size={16}/>
+                Catálogo
+            </Badge>
+            <div className="grid grid-cols-2 flex-wrap gap-8">
+                {categories.map(category => <CategoryItem key={category.id} category={category} />)}
+            </div>
+        </div>
+     );
 }
  
 export default CatalogPage;
