56131dd5e65be33ff54a77a0b9b1012ebba68b41
MRodrigues51 <marcelorodriguesweb@gmail.com>
2023-11-01 13:38:53 -0300

    feat: add DiscountBadge component

---
+++ src/components/ui/discount-badge.tsx
@@ -0,0 +1,12 @@
+import { Badge, BadgeProps } from '@/components/ui/badge';
+import { ArrowDownIcon } from 'lucide-react';
+import { twMerge } from 'tailwind-merge';
+const DiscountBadge = ({children, className, ...props}: BadgeProps) => {
+    return ( 
+        <Badge className={twMerge("px-2 py-[2px]", className)} {...props}>
+            <ArrowDownIcon size={14}/> {children}%
+          </Badge>
+     );
+}
+ 
+export default DiscountBadge;
