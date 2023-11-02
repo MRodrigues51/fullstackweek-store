4f53d7b55dcae2fbd19cfcdf653f0acb62af1c18
MRodrigues51 <marcelorodriguesweb@gmail.com>
2023-11-02 14:54:22 -0300

    feat: ensure product can be removed from cart

--- src/providers/cart.tsx
+++ src/providers/cart.tsx
@@ -1,71 +1,93 @@
-"use client"
+"use client";
 import { ProductWithTotalPrice } from "@/helpers/products";
 import { ReactNode, createContext, useState } from "react";
 
-
 export interface CartProduct extends ProductWithTotalPrice {
-    quantity: number;
-    
+  quantity: number;
 }
 
 interface ICartContext {
-    products: CartProduct[]
-    cartTotalPrice: number
-    cartBasePrice: number
-    cartTotalDiscount: number
-    addProductToCart: (product: CartProduct) => void
+  products: CartProduct[];
+  cartTotalPrice: number;
+  cartBasePrice: number;
+  cartTotalDiscount: number;
+  addProductToCart: (product: CartProduct) => void;
+  decreaseProductQuantity: (productId: String) => void;
 }
 
 export const CartContext = createContext<ICartContext>({
-    products: [],
-    cartTotalPrice: 0,
-    cartBasePrice: 0,
-    cartTotalDiscount: 0,
-    addProductToCart: () => {},
-})
+  products: [],
+  cartTotalPrice: 0,
+  cartBasePrice: 0,
+  cartTotalDiscount: 0,
+  addProductToCart: () => {},
+  decreaseProductQuantity:  () => {},
+});
 
-const CartProvider = ({children}: {children: ReactNode}) => {
-    const [products, setProducts] = useState<CartProduct[]>([])
+const CartProvider = ({ children }: { children: ReactNode }) => {
+  const [products, setProducts] = useState<CartProduct[]>([]);
 
-    const addProductToCart = (product: CartProduct) => {
-       // se o produto já estiver no carrinho, apenas aumente a sua quantidade
+  const addProductToCart = (product: CartProduct) => {
+    // se o produto já estiver no carrinho, apenas aumente a sua quantidade
     const productIsAlreadyOnCart = products.some(
-        (cartProduct) => cartProduct.id === product.id,
+      (cartProduct) => cartProduct.id === product.id,
+    );
+
+    if (productIsAlreadyOnCart) {
+      setProducts((prev) =>
+        prev.map((cartProduct) => {
+          if (cartProduct.id === product.id) {
+            return {
+              ...cartProduct,
+              quantity: cartProduct.quantity + product.quantity,
+            };
+          }
+
+          return cartProduct;
+        }),
       );
-  
-      if (productIsAlreadyOnCart) {
-        setProducts((prev) =>
-          prev.map((cartProduct) => {
-            if (cartProduct.id === product.id) {
-              return {
-                ...cartProduct,
-                quantity: cartProduct.quantity + product.quantity,
-              };
-            }
-  
-            return cartProduct;
-          }),
-        );
-  
-        return;
-      }
-  
-      // se não, adicione o produto à lista
-      setProducts((prev) => [...prev, product]);
+
+      return;
     }
 
-    
-    return ( 
-        <CartContext.Provider value={{
-            products,
-            addProductToCart,
-            cartTotalPrice: 0,
-            cartBasePrice: 0,
-            cartTotalDiscount: 0
-        }}>
-            {children}
-        </CartContext.Provider>
-     );
-}
- 
+    // se não, adicione o produto à lista
+    setProducts((prev) => [...prev, product]);
+  };
+
+  const decreaseProductQuantity = (productId: String) => {
+    // se a quantidade for 1, remova o produto do carrinho
+
+    // se não diminua a quantidade em 1
+
+    setProducts((prev) =>
+      prev
+        .map((cartProduct) => {
+          if (cartProduct.id === productId) {
+            return {
+              ...cartProduct,
+              quantity: cartProduct.quantity - 1,
+            };
+          }
+          return cartProduct;
+        })
+        .filter((cartProduct) => cartProduct.quantity > 0),
+    );
+  };
+
+  return (
+    <CartContext.Provider
+      value={{
+        products,
+        addProductToCart,
+        decreaseProductQuantity,
+        cartTotalPrice: 0,
+        cartBasePrice: 0,
+        cartTotalDiscount: 0,
+      }}
+    >
+      {children}
+    </CartContext.Provider>
+  );
+};
+
 export default CartProvider;
