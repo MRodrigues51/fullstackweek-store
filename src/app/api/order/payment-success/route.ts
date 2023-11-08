7fb38ee31cd9a0a204180dda9b3d6f20244850a2
MRodrigues51 <marcelorodriguesweb@gmail.com>
2023-11-08 06:42:07 -0300

    feat: improving user session validation and order list access logic on orderPage

--- src/app/api/order/payment-success/route.ts
+++ src/app/api/order/payment-success/route.ts
@@ -1,7 +1,7 @@
 import { prismaClient } from "@/lib/prisma"
 import { NextResponse } from "next/server"
 import Stripe from "stripe"
-import { metadata } from '../../../layout';
+
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
     apiVersion: "2023-10-16"
--- src/app/orders/page.tsx
+++ src/app/orders/page.tsx
@@ -1,32 +1,36 @@
-
 import { authOptions } from "@/lib/auth";
 import { getServerSession } from "next-auth";
 import { Badge } from "@/components/ui/badge";
 import { PackageSearchIcon } from "lucide-react";
 import { prismaClient } from "@/lib/prisma";
-import OrderItem from './components/order-item';
+import OrderItem from "./components/order-item";
 
-export const dynamic = 'force-dynamic'
+export const dynamic = "force-dynamic";
 
 async function OrderPage() {
-  const user = getServerSession(authOptions);
+  const session = await getServerSession(authOptions);
 
-  if (!user) {
-    return <>access denied</>;
+  if (!session || !session.user) {
+    return (
+      <div className="flex flex-col items-center justify-center">
+        <p className="mt-8">Acesso negado.</p>
+        <p>Faça o login para ter acesso!</p>
+      </div>
+    );
   }
 
   const orders = await prismaClient.order.findMany({
     where: {
-      userId: (user as any).id,
+      userId: session.user.id,
     },
     include: {
       orderProducts: {
         include: {
-          product: true
-        }
-      }
-    }
-  })
+          product: true,
+        },
+      },
+    },
+  });
   return (
     <div className="p-5">
       <Badge
@@ -36,11 +40,17 @@ async function OrderPage() {
         <PackageSearchIcon size={16} />
         Meus Pedidos
       </Badge>
-      <div className="flex flex-col gap-5 mt-5">
-      {orders.map(order => (<OrderItem key={order.id} order= {order}/>))}
-      </div>
+      {orders.length === 0 ? (
+        <p className="mt-5 flex flex-col gap-5">Você ainda não fez nenhuma compra, vamos às compras?</p>
+      ) : (
+        <div className="mt-5 flex flex-col gap-5">
+          {orders.map((order) => (
+            <OrderItem key={order.id} order={order} />
+          ))}
+        </div>
+      )}
     </div>
   );
-};
+}
 
 export default OrderPage;
